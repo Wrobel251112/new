1057b7a213c0c1adca573e1d56b3d022be64cb62
Wrobel251112 <251112@edu.p.lodz.pl>
2023-11-29 18:21:59 +0100

    02 Zmiana mnożenia w metodzie Policz

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main() {
     int a=2, b=3, policz;
-    policz = a+b;
+    policz = a*b;
     printf("Hello world\n");
-    printf("W pierwszym branchu; 2+3=%d", policz);
+    printf("W pierwszym branchu; 2*3=%d", policz);
     return 0;
 }
