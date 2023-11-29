802ac4385929a8b6284aab382b1745fa28dfe324
Wrobel251112 <251112@edu.p.lodz.pl>
2023-11-29 18:10:25 +0100

    01 Zmiana metody Policz

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a=2, b=3, policz;
+    policz = a+b;
+    printf("Hello world\n");
+    printf("W pierwszym branchu; 2+3=%d", policz);
     return 0;
 }
